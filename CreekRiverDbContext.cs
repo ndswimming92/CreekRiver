c88bfd8f1fb7cd2e5613ab5b6fe9297feeca5a8c
Nicholas Davidson <nicholas.davidson.20@gmail.com>
2024-02-13 19:24:49 -0600

    Adding a DbContext class & Seeding the database with data

---
+++ CreekRiverDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CreekRiver.Models;
+
+public class CreekRiverDbContext : DbContext
+{
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // seed data with campsite types
+        modelBuilder.Entity<CampsiteType>().HasData(new CampsiteType[]
+        {
+        new CampsiteType {Id = 1, CampsiteTypeName = "Tent", FeePerNight = 15.99M, MaxReservationDays = 7},
+        new CampsiteType {Id = 2, CampsiteTypeName = "RV", FeePerNight = 26.50M, MaxReservationDays = 14},
+        new CampsiteType {Id = 3, CampsiteTypeName = "Primitive", FeePerNight = 10.00M, MaxReservationDays = 3},
+        new CampsiteType {Id = 4, CampsiteTypeName = "Hammock", FeePerNight = 12M, MaxReservationDays = 7}
+        });
+    }
+
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<UserProfile> UserProfiles { get; set; }
+    public DbSet<Campsite> Campsites { get; set; }
+    public DbSet<CampsiteType> CampsiteTypes { get; set; }
+
+    public CreekRiverDbContext(DbContextOptions<CreekRiverDbContext> context) : base(context)
+    {
+
+    }
+}
